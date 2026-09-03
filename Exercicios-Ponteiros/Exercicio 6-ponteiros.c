77e9dad7200510309e0cd799f2b7a60e3d3693c2
Macedo <macedopedrocunha@gmail.com>
2026-09-03 19:54:49 -0300

    Update Exercicio 6-ponteiros.c

--- Exercicios-Ponteiros/Exercicio 6-ponteiros.c
+++ Exercicios-Ponteiros/Exercicio 6-ponteiros.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 
 int soma(int *v, int n) {
-    int s = 0;
-    int *p = v;
-    int *fim = v + n;
-
-    while (p < fim) {
-        s += *p;
-        p++;
+    int total = 0;
+    
+    for (int i = 0; i < n; i++) {
+        total += v[i];
     }
-
-    return s;
+    
+    return total;
 }
 
+int main() {
+    int v[] = {10, 20, 30, 40, 50};
+    int n = 5;
+
+    int resultado = soma(v, n);
+
+    printf("A soma dos elementos do vetor eh: %d\n", resultado);
+
+    return 0;
+}
